9d469873b4d2f651eed1c478286a5b9fa7bb3045
sathoune <michal5bargiel@gmail.com>
2023-12-01 13:55:57 +0100

    ✨ Solve part 1.

--- day-01/part1.go
+++ day-01/part1.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -17,9 +20,25 @@ func readFile(fileName string) []string {
 	return strings.Split(string(data), "\n")
 }
 
+func findFirstAndLastDigit(text string) (first int, last int, notFound error) {
+	pattern, _ := regexp.Compile(`[0-9]`)
+	result := pattern.FindAll([]byte(text), -1)
+	if len(result) == 0 {
+		return 0, 0, errors.New("not found")
+	}
+	first, _ = strconv.Atoi(string(result[0]))
+	last, _ = strconv.Atoi(string(result[len(result)-1]))
+	return
+}
+
 func main() {
-	inputName := "input.txt"
+	inputName := "day-01/input.txt"
 	formattedData := readFile(inputName)
-	fmt.Println(formattedData)
-	fmt.Println(len(formattedData))
+	sum := 0
+	for _, line := range formattedData {
+		firstInteger, lastInteger, _ := findFirstAndLastDigit(line)
+		sum += firstInteger * 10
+		sum += lastInteger
+	}
+	fmt.Println(sum)
 }
