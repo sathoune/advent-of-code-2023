da3948f17cef6aea8f7bfc65bf3eacedf2371bac
sathoune <michal5bargiel@gmail.com>
2023-12-02 13:15:57 +0100

    ✨ Solution for day 2 part 2.

--- day02/part2.go
+++ day02/part2.go
@@ -7,10 +7,34 @@ import (
 	"runtime"
 )
 
+func findMinimalBallsCount(game Game) (minimal Stage) {
+	minimal = Stage{0, 0, 0}
+	for _, stage := range game {
+		if stage.Red > minimal.Red {
+			minimal.Red = stage.Red
+		}
+		if stage.Green > minimal.Green {
+			minimal.Green = stage.Green
+		}
+		if stage.Blue > minimal.Blue {
+			minimal.Blue = stage.Blue
+		}
+	}
+	return
+}
+
 func Part2() {
 	_, thisFilepath, _, _ := runtime.Caller(0)
-	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "demo.txt")
+	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "input.txt")
 	txt := utils.ReadFile(dataFilepath)
+
 	games := parseGames(txt)
-	fmt.Println(games)
+
+	power := 0
+	for _, game := range games {
+		minimal := findMinimalBallsCount(game)
+		power += minimal.Red * minimal.Green * minimal.Blue
+	}
+
+	fmt.Println(power)
 }
