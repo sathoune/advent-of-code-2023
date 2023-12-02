1f8d8501a9e06095de7025dd12ebbe388419fd39
sathoune <michal5bargiel@gmail.com>
2023-12-02 12:54:37 +0100

    ✨ Day 2 part 1 solution.

--- day02/part1.go
+++ day02/part1.go
@@ -5,15 +5,104 @@ import (
 	"fmt"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
-var maxRed = 12
-var maxGreen = 13
-var maxBlue = 14
+type Stage struct {
+	Red   int
+	Green int
+	Blue  int
+}
+
+type Game = []Stage
+
+var limits = Stage{
+	12,
+	13,
+	14,
+}
+
+func parseId(text string) (id int) {
+	id, _ = strconv.Atoi(strings.Replace(text, "Game ", "", 1))
+	return
+}
+
+func splitPick(pick string) (count int, colour string) {
+	split := strings.Split(pick[1:], " ")
+	count, _ = strconv.Atoi(split[0])
+	colour = split[1]
+	return
+}
+
+func parseGame(text string) (result Stage) {
+	foundPicks := strings.Split(text, ",")
+
+	for _, pick := range foundPicks {
+		count, colour := splitPick(pick)
+		if colour == "blue" {
+			result.Blue = count
+		} else if colour == "red" {
+			result.Red = count
+		} else if colour == "green" {
+			result.Green = count
+		}
+	}
+	return
+}
+
+func splitIntoChunks(text string) (id int, results []Stage) {
+	parts := strings.Split(text, ":")
+	if len(parts) > 2 {
+		panic("To many colons!")
+	}
+	id = parseId(parts[0])
+	for _, gameText := range strings.Split(parts[1], ";") {
+		results = append(results, parseGame(gameText))
+	}
+
+	return
+}
+
+func validateStage(stage Stage) (valid bool) {
+	if stage.Red > limits.Red {
+		return false
+	}
+	if stage.Green > limits.Green {
+		return false
+	}
+	if stage.Blue > limits.Blue {
+		return false
+	}
+	return true
+}
+
+func validateGame(game Game) (valid bool) {
+	for _, stage := range game {
+		stageValid := validateStage(stage)
+		if !stageValid {
+			return false
+		}
+	}
+	return true
+}
 
 func Part1() {
 	_, thisFilepath, _, _ := runtime.Caller(0)
-	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "demo.txt")
+	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "input.txt")
 	txt := utils.ReadFile(dataFilepath)
-	fmt.Println(txt)
+
+	grouped := make(map[int]Game)
+	for _, line := range txt {
+		id, game := splitIntoChunks(line)
+		grouped[id] = game
+	}
+	sum := 0
+	for id, game := range grouped {
+		valid := validateGame(game)
+		if valid {
+			sum += id
+		}
+	}
+	fmt.Println(sum)
 }
