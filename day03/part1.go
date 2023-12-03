23c3b25bed3d594401c6485e978f00e3caa8bca2
sathoune <michal5bargiel@gmail.com>
2023-12-03 13:05:28 +0100

    ✨ Parse all numbers.

--- day03/part1.go
+++ day03/part1.go
@@ -1,5 +1,55 @@
 package day03
 
-func Part1() {
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"runtime"
+	"strconv"
+	"strings"
+)
 
+type Matrix = [][]string
+
+func sliceToMatrix(text []string) (matrix Matrix) {
+	for _, row := range text {
+		matrix = append(matrix, strings.Split(row, ""))
+	}
+	return
+}
+
+type Number struct {
+	row   int
+	start int
+	end   int
+	value int
+}
+
+func findDigitsInRows(matrix []string) (digits []Number) {
+	pattern := regexp.MustCompile(`\d+`)
+
+	for rowIndex, row := range matrix {
+		foundCoordinates := pattern.FindAllStringIndex(row, -1)
+		for _, coordinates := range foundCoordinates {
+			start, end := coordinates[0], coordinates[1]
+			parsedNumber, _ := strconv.Atoi(row[start:end])
+
+			digits = append(digits, Number{
+				rowIndex,
+				start,
+				end,
+				parsedNumber,
+			})
+		}
+	}
+	return
+}
+
+func Part1() {
+	_, thisFilepath, _, _ := runtime.Caller(0)
+	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "demo.txt")
+	txt := utils.ReadFile(dataFilepath)
+	digitsCoordinates := findDigitsInRows(txt)
+	fmt.Println(digitsCoordinates)
 }
