b3c56c2b3022327d035da7843e5c98954dd6f8b1
sathoune <michal5bargiel@gmail.com>
2023-12-03 13:43:39 +0100

    ✨ Solution for part1.

--- day03/part1.go
+++ day03/part1.go
@@ -7,23 +7,18 @@ import (
 	"regexp"
 	"runtime"
 	"strconv"
-	"strings"
 )
-
-type Matrix = [][]string
-
-func sliceToMatrix(text []string) (matrix Matrix) {
-	for _, row := range text {
-		matrix = append(matrix, strings.Split(row, ""))
-	}
-	return
-}
 
 type Number struct {
 	row   int
 	start int
 	end   int
 	value int
+}
+
+type Coordinate struct {
+	x int
+	y int
 }
 
 func findDigitsInRows(matrix []string) (digits []Number) {
@@ -46,10 +41,105 @@ func findDigitsInRows(matrix []string) (digits []Number) {
 	return
 }
 
+func findCoordinatesAround(
+	partCandidate Number,
+	maxX int,
+	maxY int,
+) (coordinates []Coordinate) {
+	yBeforeExists := partCandidate.row-1 >= 0
+	yAfterExists := partCandidate.row+1 < maxY
+	xBeforeExists := partCandidate.start-1 >= 0
+	xAfterExists := partCandidate.end < maxX
+
+	// Before
+	if xBeforeExists {
+		coordinates = append(coordinates, Coordinate{
+			partCandidate.start - 1,
+			partCandidate.row,
+		})
+		// Diagonal <^
+		if yBeforeExists {
+			coordinates = append(coordinates, Coordinate{
+				partCandidate.start - 1,
+				partCandidate.row - 1,
+			})
+		}
+		// Diagonal <\/
+		if yAfterExists {
+			coordinates = append(coordinates, Coordinate{
+				partCandidate.start - 1,
+				partCandidate.row + 1,
+			})
+		}
+
+	}
+	// After
+	if xAfterExists {
+		coordinates = append(coordinates, Coordinate{
+			partCandidate.end,
+			partCandidate.row,
+		})
+		// Diagonal ^>
+		if yBeforeExists {
+			coordinates = append(coordinates, Coordinate{
+				partCandidate.end,
+				partCandidate.row - 1,
+			})
+		}
+		// Diagonal \/>
+		if yAfterExists {
+			coordinates = append(coordinates, Coordinate{
+				partCandidate.end,
+				partCandidate.row + 1,
+			})
+		}
+	}
+
+	// Above
+	if yBeforeExists {
+		for x := partCandidate.start; x < partCandidate.end; x++ {
+			coordinates = append(coordinates, Coordinate{
+				x,
+				partCandidate.row - 1,
+			})
+		}
+	}
+	// Below
+	if yAfterExists {
+		for x := partCandidate.start; x < partCandidate.end; x++ {
+			coordinates = append(coordinates, Coordinate{
+				x,
+				partCandidate.row + 1,
+			})
+		}
+	}
+	return
+}
+
+func checkIfAnEnginePart(partCandidate Number, matrix []string) bool {
+	coordinates := findCoordinatesAround(
+		partCandidate,
+		len(matrix[0]),
+		len(matrix),
+	)
+	for _, coordinate := range coordinates {
+		if string(matrix[coordinate.y][coordinate.x]) != "." {
+			return true
+		}
+	}
+	return false
+}
 func Part1() {
 	_, thisFilepath, _, _ := runtime.Caller(0)
-	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "demo.txt")
+	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "input.txt")
 	txt := utils.ReadFile(dataFilepath)
+
 	digitsCoordinates := findDigitsInRows(txt)
-	fmt.Println(digitsCoordinates)
+	sum := 0
+	for _, partCandidate := range digitsCoordinates {
+		if checkIfAnEnginePart(partCandidate, txt) {
+			sum += partCandidate.value
+		}
+	}
+	fmt.Println(sum)
 }
