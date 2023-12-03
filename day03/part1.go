a1e4ccb8e990540c59a55202b9b4b538a717bb52
sathoune <michal5bargiel@gmail.com>
2023-12-03 12:04:29 +0100

    ✨ Setup for day 3.

---
+++ day03/part1.go
@@ -0,0 +1,5 @@
+package day03
+
+func Part1() {
+
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"advent-of-code-2023/cli"
 	"advent-of-code-2023/day01"
 	"advent-of-code-2023/day02"
+	"advent-of-code-2023/day03"
 	"fmt"
 )
 
@@ -15,6 +16,10 @@ var availableSolutions = map[int]map[int]func(){
 	2: {
 		1: day02.Part1,
 		2: day02.Part2,
+	},
+	3: {
+		1: day03.Part1,
+		2: day03.Part2,
 	},
 }
 
