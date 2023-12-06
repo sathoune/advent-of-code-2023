2c46398d1ed807e73394c300cb90a02ade1004a3
sathoune <michal5bargiel@gmail.com>
2023-12-06 16:46:05 +0100

    ✨ Day 6 part 2 solution.

--- day06/part2.go
+++ day06/part2.go
@@ -1,5 +1,42 @@
 package day06
 
-func Part2() {
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+)
 
+func parseDataAsSingleInt(text string) (parsed int) {
+	parts := strings.Split(text, ": ")
+	numberString := strings.Replace(
+		parts[1],
+		" ",
+		"",
+		-1,
+	)
+	if numberString != "" {
+		parsed, _ = strconv.Atoi(numberString)
+	}
+	return
+}
+
+func Part2() {
+	_, thisFilepath, _, _ := runtime.Caller(0)
+	dataFilepath := filepath.Join(filepath.Dir(thisFilepath), "input.txt")
+	data := utils.ReadFile(dataFilepath)
+
+	topScores := parseTopScores(
+		[]int{parseDataAsSingleInt(data[0])},
+		[]int{parseDataAsSingleInt(data[1])},
+	)
+
+	ans := 1
+	for _, topScore := range topScores {
+		solutions := findStrategies(topScore)
+		ans *= solutions
+	}
+	fmt.Println(ans)
 }
